c853468506793d6adf6b8414878649b5c1d7dc62
Guillaume Jacquart <guillaume.jacquart@live.fr>
2017-05-15 09:12:21 +0000

    Change leveldb to dirty for compatibility issues

--- api/routes/api.js
+++ api/routes/api.js
@@ -1,37 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
-var levelup = require('level')
-
-// 1) Create our database, supply location and options.
-//    This will create or open the underlying LevelDB store.
-var db = levelup('./data')
+var dirty = require('dirty');
+var path = require('path');
+var db = dirty(path.join(__dirname, '../data/cache.db'));
 
 /* GET saved url listing. */
 router.get('/url', function(req, res, next) {
-    db.get('url', function(err, value) {
-        if (err) return res.status(404).json(err) // some kind of I/O error
+    var url = db.get('url')
+    if (!url) return res.status(404) // some kind of I/O error
 
-        return res.json({
-            status: 'ok',
-            url: value
-        })
+    return res.json({
+        status: 'ok',
+        url: url
     })
 });
 
 /* GET users listing. */
 router.put('/url', function(req, res, next) {
     var url = req.body.url;
-    if(!url){
+    if (!url) {
         return res.status(400).json({
             message: 'Incorrect url'
         })
     }
 
-    db.put('url', url, function(err) {
-        if (err) return res.status(500).json(err) // some kind of I/O error
-
+    db.set('url', url, function() {
         return res.json({
             status: 'ok',
             url: url
@@ -41,26 +35,22 @@ router.put('/url', function(req, res, next) {
 
 /* GET providers info from traefik. */
 router.get('/providers', function(req, res, next) {
-    db.get('url', function(err, value) {
+    var url = db.get('url');
+    if (!url) {
+        // handle a 'NotFoundError' here
+        return res.status(404).json({
+            message: 'traefik url not provided'
+        })
+    }
+
+    var x = request(url + '/api/providers', function(err, response) {
         if (err) {
-            if (err.notFound) {
-                // handle a 'NotFoundError' here
-                return res.status(404).json({
-                    message: 'traefik url not provided'
-                })
-            }
-            // I/O or other error, pass it up the callback chain
             return res.status(500).json(err);
         }
-        
-        var x = request(value + '/api/providers', function(err, response){
-            if(err){
-                return res.status(500).json(err);
-            }
-        });
-        req.pipe(x)
-        x.pipe(res)
-    })
+    });
+    req.pipe(x)
+    x.pipe(res)
 });
+
 
 module.exports = router;
